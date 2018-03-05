1f5917b2b6b67248a9c9fa8fbae91edebbf8c639
edussuriyac <edussuriyac@gmail.com>
2018-03-05 10:24:26 +0530

    mobile app

--- src/main/java/com/server/rpms/rpms_server_mobileapp/MainActivity.java
+++ src/main/java/com/server/rpms/rpms_server_mobileapp/MainActivity.java
@@ -1,13 +1,49 @@
 package com.server.rpms.rpms_server_mobileapp;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText ssid,password,host;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.submit);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JSONObject postData = new JSONObject();
+                try {
+                    ssid = (EditText)findViewById(R.id.ssid);
+                    password = (EditText)findViewById(R.id.password);
+                    host = (EditText)findViewById(R.id.host);
+
+                    postData.put("ssid", ssid.getText().toString());
+                    postData.put("password", password.getText().toString());
+                    postData.put("host", host.getText().toString());
+
+
+                    //new SendDeviceDetails.execute("http://52.88.194.67:8080/IOTProjectServer/registerDevice", postData.toString());
+                    new SendDeviceDetails().execute("http://192.168.4.1", postData.toString());
+                   // new SendDeviceDetails.execute();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
---
+++ src/main/java/com/server/rpms/rpms_server_mobileapp/SendDeviceDetails.java
@@ -0,0 +1,98 @@
+package com.server.rpms.rpms_server_mobileapp;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import android.content.Context;
+
+public class SendDeviceDetails extends AsyncTask<String,Void,String> {
+    //Context context;
+
+    //SendDeviceDetails (Context ctx) {context=ctx;}
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        String data = "";
+
+        HttpURLConnection httpURLConnection = null;
+        try {
+
+            httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+            httpURLConnection.setRequestMethod("POST");
+
+            httpURLConnection.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+            wr.writeBytes("PostData=" + params[1]);
+            wr.flush();
+            wr.close();
+
+            InputStream in = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+            int inputStreamData = inputStreamReader.read();
+            while (inputStreamData != -1) {
+                char current = (char) inputStreamData;
+                inputStreamData = inputStreamReader.read();
+                data += current;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (httpURLConnection != null) {
+                httpURLConnection.disconnect();
+            }
+        }
+
+        return data;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Log.e("TAG", result); // this is expecting a response code to be sent from your server upon receiving the POST data
+    }
+
+
+//    protected void onPreExcecute(){
+//        alertDialog=new AlertDialog.Builder(context).create();
+//        alertDialog.setTitle("LOGIN STATUS");
+//    }
+//
+//
+//
+//
+//    protected void onPostExcecute(String result){
+//        alertDialog.setMessage(result);
+//        alertDialog.show();
+//
+//    }
+//
+//
+//    protected void onProgress(Void... values){
+//        super.onProgressUpdate(values);
+//    }
+
+}
